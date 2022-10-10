fd76b2a1312153c31c209d1ce9483e2f019f4797
hazmole <buzzfirefox@gmail.com>
2022-10-10 21:54:34 +0800

    20221010: big Update & KamigaKari-SDL

---
+++ Kamigakari-StoryOfDreamland/index.js
@@ -0,0 +1,19 @@
+var TITLE = "【神我狩】夢之國物語 - 團錄";
+var INDEX = [
+	{
+		title: "場景１～２",
+		url: "20220929-Kamigakari-StoryOfDreamland.html",
+	},
+	{
+		title: "場景３",
+		url: "20221004-Kamigakari-StoryOfDreamland.html",
+	},
+	{
+		title: "場景４",
+		url: "20221003-Kamigakari-StoryOfDreamland.html",
+	},
+	{
+		title: "場景５",
+		url: "20221006-Kamigakari-StoryOfDreamland.html",
+	},
+];
