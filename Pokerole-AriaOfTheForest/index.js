0043d2c09185f0431206632c850573b812f293ac
hazmole <buzzfirefox@gmail.com>
2022-10-12 01:06:58 +0800

    20221012: updateRP: Pokerole-AriaOfTheForest

---
+++ Pokerole-AriaOfTheForest/index.js
@@ -0,0 +1,8 @@
+var TITLE = "【Pokerole】森林的詠嘆調 - 團錄";
+var INDEX = [
+	{ title: "第一次", url: "20210812-AriaOfTheForest.html", },
+	{ title: "第二次", url: "20210819-AriaOfTheForest.html", },
+	{ title: "第三次", url: "20210904-AriaOfTheForest.html", },
+	{ title: "第四次", url: "20210909-AriaOfTheForest.html", },
+	{ title: "第五次", url: "20210916-AriaOfTheForest.html", },
+];
