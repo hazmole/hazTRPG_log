0ba87b6eb4dc574c7b23ecad27f23960bbeb5f9f
hazmole <buzzfirefox@gmail.com>
2022-10-12 00:41:21 +0800

    20221012: Pokerole-RangerQuest

---
+++ Pokerole-RangerQuest/index.js
@@ -0,0 +1,6 @@
+var TITLE = "【Pokerole】巡護員的考驗 - 團錄";
+var INDEX = [
+	{ title: "第一次", url: "20210422-Pokerole-RangerQuest.html", },
+	{ title: "第二次", url: "20210429-Pokerole-RangerQuest.html", },
+	{ title: "第三次", url: "20210520-Pokerole-RangerQuest.html", },
+];
