b194d957f92aef37ff2f429d1e9e78b111018fcc
hazmole <buzzfirefox@gmail.com>
2022-10-13 23:47:14 +0800

    20221013: SDL05

--- Kamigakari-StoryOfDreamland/index.js
+++ Kamigakari-StoryOfDreamland/index.js
@@ -4,4 +4,5 @@ var INDEX = [
 	{ title: "場景３", url: "20221004-Kamigakari-StoryOfDreamland.html", },
 	{ title: "場景４", url: "20221003-Kamigakari-StoryOfDreamland.html", },
 	{ title: "場景５", url: "20221006-Kamigakari-StoryOfDreamland.html", },
+	{ title: "場景６、７", url: "20221013-Kamigakari-StoryOfDreamland.html", },
 ];
