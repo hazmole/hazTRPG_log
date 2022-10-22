220db2d3fe9ab70a9ba95c08e4ee197e29f42d9e
hazmole <buzzfirefox@gmail.com>
2022-10-22 16:01:32 +0800

    20221022: add site index

---
+++ index.js
@@ -0,0 +1,8 @@
+var INDEX = [
+	{ system: "神話創世", 		title: "炎之森", 		url: "Amadeus-FireForest", },
+	{ system: "神話創世", 		title: "天堂謳歌", 		url: "Amadeus-HeavenSong", },
+	{ system: "神我狩", 			title: "夢之國物語", 	url: "Kamigakari-StoryOfDreamland", },
+	{ system: "FATE", 			title: "耶路撒冷聖杯戰爭", url: "Fate-Jerusalem", },
+	{ system: "Pokerole-2.0", 	title: "森林的詠嘆調", url: "Pokerole-AriaOfTheForest", },
+	{ system: "Pokerole-2.0", 	title: "巡護員的考驗", url: "Pokerole-RangerQuest", },
+];
