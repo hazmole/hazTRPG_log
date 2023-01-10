c38b023ecc5e456d9397626da2d9b1af95e090a8
hazmole <buzzfirefox@gmail.com>
2023-01-10 22:10:28 +0800

    HeianSakabadouElegy

---
+++ Kamigakari-HeianSakabadouElegy/index.js
@@ -0,0 +1,6 @@
+var TITLE = "【神我狩】平安逆刃哀歌 - 團錄";
+var INDEX = [
+	{ title: "導入", url: "20221215-Kamigakari-HeianSakabadouElegy.html", },
+	{ title: "遭遇戰", url: "20221229-Kamigakari-HeianSakabadouElegy.html", },
+	{ title: "轉折", url: "20230105-Kamigakari-HeianSakabadouElegy.html", },
+];
