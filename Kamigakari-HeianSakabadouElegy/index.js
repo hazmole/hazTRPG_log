77786a424bf106d4e46776b9a7ef71b165b06d33
hazmole <buzzfirefox@gmail.com>
2023-03-08 22:02:21 +0800

    20230308: Kamigakari-HeianSakabadouElegy

--- Kamigakari-HeianSakabadouElegy/index.js
+++ Kamigakari-HeianSakabadouElegy/index.js
@@ -3,4 +3,8 @@ var INDEX = [
 	{ title: "導入", url: "20221215-Kamigakari-HeianSakabadouElegy.html", },
 	{ title: "遭遇戰", url: "20221229-Kamigakari-HeianSakabadouElegy.html", },
 	{ title: "轉折", url: "20230105-Kamigakari-HeianSakabadouElegy.html", },
+	{ title: "破局", url: "20230112-Kamigakari-HeianSakabadouElegy.html", },
+	{ title: "矯正", url: "20230126-Kamigakari-HeianSakabadouElegy.html", },
+	{ title: "最終戰", url: "20230209-Kamigakari-HeianSakabadouElegy.html", },
+	{ title: "結尾", url: "20230216-Kamigakari-HeianSakabadouElegy.html", },
 ];
