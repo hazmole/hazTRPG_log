7029b086d6ab4b7da7fcd9e56673de90319dcb99
hazmole <buzzfirefox@gmail.com>
2024-10-26 15:23:33 +0800

    create folder of Monsterhearts_HeartOfHeltonHigh

---
+++ Monsterhearts_HeartOfHeltonHigh/index.js
@@ -0,0 +1,5 @@
+var TITLE = "【怪物心】海爾頓高校記事 - 團錄";
+var INDEX = [
+	{ title: "第一次", url: "20241017-Monsterhearts_HeartOfHeltonHigh.html", },
+	{ title: "第二次", url: "20241024-Monsterhearts_HeartOfHeltonHigh.html", },
+];
