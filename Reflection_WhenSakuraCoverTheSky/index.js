93f696798e0bcdb8e88569cedadddaa5c221be8e
hazmole <buzzfirefox@gmail.com>
2024-10-26 16:13:18 +0800

    fixed metadata

---
+++ Reflection_WhenSakuraCoverTheSky/index.js
@@ -0,0 +1,4 @@
+var TITLE = "【宿命倒影】櫻花遮蔽的天幕 - 團錄";
+var INDEX = [
+	{ title: "第一次", url: "20231217-Reflection-WhenSakuraCoverTheSky.html", },
+];
--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 var INDEX = [
-	{ system: "神話創世", 		title: "炎之森", 		url: "Amadeus-FireForest", },
-	{ system: "神話創世", 		title: "天堂謳歌", 		url: "Amadeus-HeavenSong", },
-	{ system: "神我狩", 			title: "夢之國物語", 	url: "Kamigakari-StoryOfDreamland", },
-	{ system: "FATE", 			title: "耶路撒冷聖杯戰爭", url: "Fate-Jerusalem", },
-	{ system: "Pokerole-2.0", 	title: "森林的詠嘆調", url: "Pokerole-AriaOfTheForest", },
-	{ system: "Pokerole-2.0", 	title: "巡護員的考驗", url: "Pokerole-RangerQuest", },
+	{
+		system: "神話創世",
+		title: "炎之森",
+		url: "Amadeus-FireForest"
+	},
+	{
+		system: "神話創世",
+		title: "天堂謳歌",
+		url: "Amadeus-HeavenSong"
+	},
+	{
+		system: "神我狩",
+		title: "夢之國物語",
+		url: "Kamigakari-StoryOfDreamland"
+	},
+	{
+		system: "FATE",
+		title: "耶路撒冷聖杯戰爭",
+		url: "Fate-Jerusalem"
+	},
+	{
+		system: "Pokerole-2.0",
+		title: "森林的詠嘆調",
+		url: "Pokerole-AriaOfTheForest"
+	},
+	{
+		system: "Pokerole-2.0",
+		title: "巡護員的考驗",
+		url: "Pokerole-RangerQuest"
+	},
+	{
+		system: "宿命倒影",
+		title: "櫻花遮蔽的天幕",
+		url: "Reflection_WhenSakuraCoverTheSky"
+	},
+	{
+		system: "怪物心",
+		title: "海爾頓高校記事",
+		url: "Monsterhearts_HeartOfHeltonHigh"
+	},
 ];
