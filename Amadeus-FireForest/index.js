3d60419fcdae6ab5339b57b7213827c889deaf40
Daniel.Huang(黃主同) <Daniel.Huang@infortrend.com>
2025-08-22 17:50:22 +0800

    upgrade index.html layout for all

--- Amadeus-FireForest/index.js
+++ Amadeus-FireForest/index.js
@@ -1,6 +1,6 @@
 var TITLE = "【神話創世】炎之森 - 團錄";
 var INDEX = [
-	{ title: "Part 1", url: "20160208_FireForest.html", },
-	{ title: "Part 2", url: "20160211_FireForest.html", },
-	{ title: "Part 3", url: "20160212_FireForest.html", },
+	{ title: "導入／第一巡迴（上）", url: "20160208_FireForest.html", },
+	{ title: "第一巡迴（下）／決戰階段", url: "20160211_FireForest.html", },
+	{ title: "後日談", url: "20160212_FireForest.html", },
 ];
