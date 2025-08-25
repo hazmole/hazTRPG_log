b0366f386406a73623b3819d7329d50172bc877d
hazmole <buzzfirefox@gmail.com>
2025-08-26 00:32:41 +0800

    redesign welcome page, update info.js

--- index.js
+++ index.js
@@ -2,41 +2,96 @@ var INDEX = [
 	{
 		system: "神話創世",
 		title: "炎之森",
-		url: "Amadeus-FireForest"
+		url: "Amadeus-FireForest",
+		imgUrl: "src/img/replay-cover/Amadeus-FireForest.png",
+		info: {
+			system: "神話創世RPG アマデウス",
+			gm: "Hazmole",
+			member: "路人鳥、Artis、誠誠、零崎七夜",
+			startDate: "2016/02/08",
+		}
 	},
 	{
 		system: "神話創世",
 		title: "天堂謳歌",
-		url: "Amadeus-HeavenSong"
+		url: "Amadeus-HeavenSong",
+		imgUrl: "src/img/replay-cover/Amadeus-HeavenSong.png",
+		info: {
+			system: "神話創世RPG アマデウス",
+			gm: "Hazmole",
+			member: "薩托里、木骨、Jimi",
+			startDate: "2017/08/13",
+		}
 	},
 	{
 		system: "神我狩",
 		title: "夢之國物語",
-		url: "Kamigakari-StoryOfDreamland"
+		url: "Kamigakari-StoryOfDreamland",
+		imgUrl: "src/img/replay-cover/StoryOfDreamland.png",
+		info: {
+			system: "武装伝奇RPG 神我狩",
+			gm: "Hazmole",
+			member: "格露、深雪、高高、41",
+			startDate: "2022/09/29",
+		}
 	},
 	{
 		system: "FATE",
 		title: "耶路撒冷聖杯戰爭",
-		url: "Fate-Jerusalem"
+		url: "Fate-Jerusalem",
+		imgUrl: "src/img/replay-cover/HolyGrail.png",
+		info: {
+			system: "命運(FATE) - 房規版",
+			gm: "Hazmole",
+			member: "紫苑翔雲、乙名",
+			startDate: "2019/12/19",
+		}
 	},
 	{
 		system: "Pokerole 2.0",
 		title: "森林的詠嘆調",
-		url: "Pokerole-AriaOfTheForest"
+		url: "Pokerole-AriaOfTheForest",
+		imgUrl: "src/img/replay-cover/Pokerole-AriaOfTheForest.png",
+		info: {
+			system: "Pokerole 2.0",
+			gm: "Hazmole",
+			member: "赤の剣戦王、流星之中、jeffary",
+			startDate: "2021/08/12",
+		}
 	},
 	{
 		system: "Pokerole 2.0",
 		title: "巡護員的考驗",
-		url: "Pokerole-RangerQuest"
+		url: "Pokerole-RangerQuest",
+		imgUrl: "src/img/replay-cover/Pokerole-RangerQuest.png",
+		info: {
+			system: "Pokerole 2.0",
+			gm: "Hazmole",
+			member: "ttk12345",
+			startDate: "2021/04/22",
+		}
 	},
 	{
 		system: "宿命倒影",
 		title: "櫻花遮蔽的天幕",
-		url: "Reflection_WhenSakuraCoverTheSky"
+		url: "Reflection_WhenSakuraCoverTheSky",
+		imgUrl: "src/img/replay-cover/Reflection_WhenSakuraCoverTheSky.png",
+		info: {
+			system: "宿命倒影 Reflection",
+			member: "Hazmole、高高",
+			startDate: "2023/12/17",
+		}
 	},
 	{
 		system: "怪物心",
 		title: "海爾頓高校記事",
-		url: "Monsterhearts_HeartOfHeltonHigh"
+		url: "Monsterhearts_HeartOfHeltonHigh",
+		imgUrl: "src/img/replay-cover/Monsterhearts_HeartOfHeltonHigh.png",
+		info: {
+			system: "怪物心2 Monsterhearts2",
+			gm: "Hazmole",
+			member: "格露、高高",
+			startDate: "2024/10/17",
+		}
 	},
 ];
