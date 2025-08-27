9f22b66fc94f65f27ee71f5e56b377a2b8f609b8
Daniel.Huang(黃主同) <Daniel.Huang@infortrend.com>
2025-08-27 17:30:59 +0800

    update replay: CrystalInTheSand

---
+++ FF14-CrystalInTheSand/index.js
@@ -0,0 +1,7 @@
+var TITLE = "【FF14】塵沙中的水晶 - 團錄彙整";
+var INDEX = [
+	{	title: "開場", url: "20241222-FF14-CrystalInTheSand-01.html" },
+	{	title: "案件調查", url: "20241228-FF14-CrystalInTheSand-02.html" },
+	{	title: "洞窟之中", url: "20250107-【FF14】塵沙中的水晶[main].html" },
+	{	title: "激戰與結尾", url: "20250123-【FF14】塵沙中的水晶[main].html" },
+];
