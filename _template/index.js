961fd76001d5b3615b5c6a6a5e2e76089306cd32
hazmole <buzzfirefox@gmail.com>
2025-08-30 16:15:23 +0800

    update _template, add memberGroup example

--- _template/index.js
+++ _template/index.js
@@ -1,5 +1,14 @@
 var TITLE = "(標題文字)";
 var INDEX = [
-	{	title: "Github(外部連結)", url: "https://github.com/hazmole/hazTRPG_log" },
-	{	title: "範例頁面(內部連結)", url: "example.html" },
+	{	title: "範例頁面", url: "example.html" },
+	{ title: "群組 (第一層)", members: [
+		{	title: "範例頁面 1", url: "example.html" },
+		{ title: "群組 (第二層)", members: [
+			{	title: "範例頁面 2", url: "example.html" },
+			{	title: "範例頁面 3", url: "example.html" },
+			{	title: "範例頁面 4", url: "example.html" },
+		]}
+	]},
+	{	title: "免費開源TRPG中文化團隊(外部連結)", url: "https://hazmole.github.io/Free-Open-TRPG-Translation/" },
+	
 ];
