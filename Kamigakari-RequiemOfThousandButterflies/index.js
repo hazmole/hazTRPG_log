867f2e9b2cb2ae3a4e9ac22365fcd15e055c84be
freeandopentrpg-boop <freeandopentrpg@gmail.com>
2025-11-12 00:07:00 +0800

    update replay: Kamigakari-RequiemOfThousandButterflies

--- Kamigakari-RequiemOfThousandButterflies/index.js
+++ Kamigakari-RequiemOfThousandButterflies/index.js
@@ -2,4 +2,6 @@ var TITLE = "【神我狩/魔道書】千蝶殘響 - 團錄彙整";
 var INDEX = [
 	{	title: "個人導入(上)", url: "20251022-Kamigakari-RequiemOfThousandButterflies.html" },
 	{	title: "個人導入(下)", url: "20251026-Kamigakari-RequiemOfThousandButterflies.html" },
+	{	title: "分科會",       url: "20251029-Kamigakari-RequiemOfThousandButterflies.html" },
+	{	title: "情報統整",     url: "20251105-Kamigakari-RequiemOfThousandButterflies.html" },
 ];
